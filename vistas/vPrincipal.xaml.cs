d8f4fc3aacaa66453a010cc50286b566e32cf47c
CristianGuamba <e1750024067@uisrael.edu.ec>
2024-06-04 21:55:13 -0500

    primer commit interfaz grafica completa

---
+++ vistas/vPrincipal.xaml.cs
@@ -0,0 +1,15 @@
+namespace cguambaS2.vistas;
+
+public partial class vPrincipal : ContentPage
+{
+	public vPrincipal()
+	{
+		InitializeComponent();
+	}
+
+    private void btnCalcular_Clicked(object sender, EventArgs e)
+    {
+       
+
+    }
+}
